021fc45c93706fae0337ceef3cf3093352613ff4
privatechen <privatechen@163.com>
2026-09-09 17:36:59 +0800

    add price watch list response dto

---
+++ src/main/java/com/fishingtime/pricewatch/dto/PriceWatchListItemResponse.java
@@ -0,0 +1,21 @@
+package com.fishingtime.pricewatch.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class PriceWatchListItemResponse {
+    private Long watchId;
+    private String platform;
+    private String skuId;
+    private String productUrl;
+    private BigDecimal purchasePrice;
+    private Integer watchDays;
+    private LocalDateTime startAt;
+    private LocalDateTime endAt;
+    private Integer status;
+}
