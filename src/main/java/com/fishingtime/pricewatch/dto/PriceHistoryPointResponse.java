012f0d27a9a8aa6c31b3cf670e5b2c3d3fa9d325
privatechen <privatechen@163.com>
2026-09-10 11:43:39 +0800

    feat: add daily price history endpoint

---
+++ src/main/java/com/fishingtime/pricewatch/dto/PriceHistoryPointResponse.java
@@ -0,0 +1,18 @@
+package com.fishingtime.pricewatch.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PriceHistoryPointResponse {
+    private LocalDate date;
+    private BigDecimal price;
+}
